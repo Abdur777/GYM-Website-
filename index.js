7807cace5c47fb916ed5479639a9477544b87d78
Abdur777 <96384603+Abdur777@users.noreply.github.com>
2022-09-18 01:40:54 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+document.getElementById("p1").classList.add("blur");
+setTimeout(function() {
+document.getElementsByClassName("column")[0].classList.add("time");
+}, 250);
+setTimeout(function() {
+   document.getElementsByClassName("column")[1].classList.add("time");
+  }, 500);
+  setTimeout(function() {
+    document.getElementsByClassName("column")[2].classList.add("time");
+  }, 1000);
+  // setTimeout(function() {
+    // document.getElementsByClassName("column")[3].classList.add("time");
+  // }, 1000);
+  setTimeout(function() {
+    document.getElementsByClassName("column")[0].classList.add("life");
+   }, 1250);
+  
+  // document.getElementsByClassName("column")[0].classList.add("life");
+  setTimeout(function() {
+      document.getElementsByClassName("column")[1].classList.add("life");
+    }, 1250);
+    setTimeout(function() {
+      document.getElementsByClassName("column")[2].classList.add("life");
+    }, 1500);
+    setTimeout(function() {
+      // document.getElementsByClassName("column")[3].classList.add("life");
+    }, 1750);
+    setTimeout(function() {
+      // document.getElementsByClassName("column")[4].classList.add("life");
+    }, 2000);
+    
+    
